f8c7e6726892a04c4fdadc5debf61767dde0a8ba
noir <avogt690@gmail.com>
2019-02-24 06:52:40 -0600

    Posts
    -Can post posts
    -Check console log, has problems posting divs to website

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ $( document ).ready(function() {
       //When this button (with the id btnSignup) is clicked, it will attempt to sign up the user
       signUser ();
    });
+   $("#submitPost").click(function(){
+      postUser();
+   });
    
 });
 function lauth(f) {
@@ -74,8 +77,10 @@ function loginUser() {
       console.log("Password matches!")
       alert("Found!")
       foundUser.lastLoginDate = moment();
+      var userId = foundUser.userId
       //Sets login cookie
       sessionStorage.setItem('status','loggedIn') 
+      sessionStorage.setItem('userId', JSON.stringify(userId))
       console.log("After log");
       tryRedirect(1);
       
@@ -111,6 +116,9 @@ function signUser() {
       localStorage.setItem("topicsArray",JSON.stringify(topics));
       localStorage.setItem("postsArray",JSON.stringify(posts));
       localStorage.setItem("tagsArray",JSON.stringify(tags));
+
+      sessionStorage.setItem('status','loggedIn')
+      sessionStorage.setItem('userId', JSON.stringify(userId)) 
       tryRedirect(1);
   }
   else{
@@ -118,6 +126,103 @@ function signUser() {
     tryRedirect(0);
   }
   
+}
+
+function postUser() {
+  var title = $("#title").val();
+  var description = $("#description").val();
+  var currentDate = moment().toISOString();
+
+  var temp = sessionStorage.getItem('userId');
+  console.log(temp); //Successful, retrieved "2";
+  var viewId = $.parseJSON(temp);
+  console.log(viewId); //Successful, retrieved 2;
+  //var foundUser = users.find(x => x.viewId === viewId);
+  
+  if (viewId == null) {
+    console.log("Whoever you are, you can't post!")
+  }
+  else {
+    function getData() {
+      var userData = localStorage.getItem('usersArray', JSON.stringify(users));
+      var viewData = $.parseJSON(userData);
+      for (var i = 0; i < viewData.length; i++) {
+        if (viewData[i].userId == viewId) {
+          return(viewData[i].email);
+        }
+      }
+    }
+    getData();
+    console.log(getData());
+    var newPost = {title: title, description: description}
+    //Need: Get the user ID from the user
+
+
+    var div2 = document.createElement("div");
+    var te = document.createTextNode("Hello");
+    div2.appendChild(te);
+    console.log(div2);
+    
+
+
+
+    newPost.postId = posts.length + 1;
+    newPost.createdDate = currentDate;
+    newPost.userId = viewId;
+    posts.push(newPost);
+
+    localStorage.setItem("usersArray",JSON.stringify(users));
+    localStorage.setItem("topicsArray",JSON.stringify(topics));
+    localStorage.setItem("postsArray",JSON.stringify(posts));
+    localStorage.setItem("tagsArray",JSON.stringify(tags));
+    console.log(newPost);
+    
+    //Post to HTML
+    
+      
+      /*var div = document.createElement("div");
+      var h2 = document.createElement("h2");
+      var p = document.createElement("p");
+      var h2node = document.createTextNode(title);
+      h2.appendChild(h2node);
+      var pnode = document.createTextNode(description);
+      p.appendChild(pnode);
+      div.appendChild(h2);
+      div.appendChild(p);
+      console.log(div);*/
+      // all of your code goes in here
+      // it runs after the DOM is built
+  
+    
+
+
+    
+    
+  }
+}
+
+function displayPosts() {
+  var allPosts = localStorage.getItem('postsArray', JSON.stringify(posts));
+      var viewPosts = $.parseJSON(allPosts);
+      for (var i = 0; i < viewPosts.length; i++) {
+
+        var ititle = viewPosts[i].title;
+        var idescription = viewPosts[i].description;
+
+        var div = document.createElement("div");
+        var h2 = document.createElement("h2");
+        var p = document.createElement("p");
+        var h2node = document.createTextNode(ititle);
+        h2.appendChild(h2node);
+        var pnode = document.createTextNode(idescription);
+        p.appendChild(pnode);
+        div.appendChild(h2);
+        div.appendChild(p);
+        var breakb = document.createElement("br");
+
+        console.log(div);
+      }
+
 }
 
 function tryRedirect(f) {
